29c89a3cbf9792d5dd6676ddf862b1d086f24aa6
Julien Linget <julien.linget@protonmail.com>
2022-08-09 20:45:58 +0200

    feat: Set up main file

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from golden_rectangle import draw_golden_rectangle
 import turtle
 
+from golden_spiral import draw_golden_spiral
+
 pen = turtle.Turtle(visible=False)
-draw_golden_rectangle(pen, 300, 10)
+
+current_base_height = 600
+nb_iteration = 15
+
+pen.speed(150)
+
+draw_golden_rectangle(pen, current_base_height, nb_iteration)
+
+pen.speed(10)
+
+draw_golden_spiral(pen, current_base_height, nb_iteration)
 
 turtle.Screen().exitonclick()
